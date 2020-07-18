d659cd2a9ac800fa543c82c67fb0fb0723dc3d07
sashitani ayumi <sasitani_611@yahoo.co.jp>
2020-07-19 00:57:36 +0900

    不要な処理を削除

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -1,12 +1,10 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.example.testjson.Data;
 import com.example.testjson.Service;
@@ -21,8 +19,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         Thread thread = new Thread(new Runnable() {
             @Override
@@ -40,51 +36,23 @@ public class SecondActivity extends AppCompatActivity {
 
                         runOnUiThread(new Runnable() {
 
+                            @SuppressLint("SetTextI18n")
                             @Override
                             public void run() {
-
                                 userIdView.setText("userId : " + data.getUserId());
                                 idView.setText("id : " + data.getId());
                                 titleView.setText("title : " + data.getTitle());
                                 bodyView.setText("body : " + data.getBody());
-                                // Stuff that updates the UI
-
                             }
                         });
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (JSONException e) {
+                    } catch (IOException | JSONException e) {
                         e.printStackTrace();
                     }
-                    //Your code goes here
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
         thread.start();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
